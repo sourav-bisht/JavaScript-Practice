a5cfb0b482ca9f640e3ebf8a0ca2142610587a0a
Sourav Bisht <souravbishthere@gmail.com>
2024-09-29 19:36:33 +0000

    30sept

---
+++ Basics/num-stringConfusion.js
@@ -0,0 +1,10 @@
+console.log(2+2)
+console.log("2"+2)
+console.log("2 + 2" + 2 + 2)
+console.log(2 + "2" + 2)
+console.log(2 + 2 + "2")
+console.log(undefined*98)
+/*
+    Until the string is encountered the addition will actually sum up all the values, but when
+    the first string is encountered then after that only string concatenation will happen.
+*/
